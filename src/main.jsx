0baad538a263fc2c880f605212871c8bdba010ba
ManyaValecha <145307455+ManyaValecha@users.noreply.github.com>
2026-06-23 10:59:46 +0530

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import AuthProvider from './auth/AuthProvider'
-import AppProvider from './contexts/AppContext'
-import App from './App'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
 import './index.css'
+import App from './App.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <AuthProvider>
-        <AppProvider>
-          <App />
-        </AppProvider>
-      </AuthProvider>
-    </BrowserRouter>
-  </React.StrictMode>
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
 )
